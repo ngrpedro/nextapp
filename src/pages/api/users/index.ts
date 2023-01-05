5d74b5b517dd3000a8511d5f6d4836bed949794a
Pedro Soares <ngrpedro22@gmail.com>
2023-01-05 13:31:45 -0300

    types

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handlerUsers(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   return res.status(200).json({
     data: [
       { id: 1, name: "Pedro" },
