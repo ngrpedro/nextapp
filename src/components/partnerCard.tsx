aa241c2703b5862d25ea86cf8569dc42f682fb47
Pedro Soares <ngrpedro22@gmail.com>
2023-01-06 19:23:01 -0300

    partnerCard init

---
+++ src/components/partnerCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PartnerCard = () => {
+  return <div>PartnerCard</div>;
+};
+
+export default PartnerCard;
--- src/components/sections/partnerView.tsx
+++ src/components/sections/partnerView.tsx
@@ -40,9 +40,9 @@ const PartnerView = () => {
         </div>
       </div>
 
-      <div className="my-10">
+      <div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
         {partners.map((pertner) => (
-          <PartnerCard />   
+          <PartnerCard />
         ))}
       </div>
     </div>
