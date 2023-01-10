40766469acf8264f2b8a4585c0b1e3207a9bb835
Pedro Soares <ngrpedro22@gmail.com>
2023-01-10 10:08:07 -0300

    style:: breadcrumb

--- src/pages/partners/[id].tsx
+++ src/pages/partners/[id].tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { CaretRight } from "phosphor-react";
+import { CaretRight, HouseSimple } from "phosphor-react";
 import React from "react";
 
 const Profile = () => {
@@ -13,16 +13,31 @@ const Profile = () => {
         <title>Parceiro</title>
       </Head>
 
-      <main /* style={{backgroundImage: }} */>
-        <div className="flex items-center justify-start gap-4">
-          <p className="text-gray-700 hover:text-gray-500 cursor-pointer transition-all">
-            Inicio
-          </p>
-          <CaretRight size={16} />
-          <p className="text-gray-700 hover:text-gray-500 cursor-pointer transition-all">
-            Perfil
-          </p>
-        </div>
+      <main>
+        <nav className="flex mb-5" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
+              <a
+                href="#"
+                className="inline-flex items-center gap-1 text-sm font-medium text-gray-700 hover:text-blue-600"
+              >
+                <HouseSimple size={20} />
+                Inicio
+              </a>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <CaretRight size={16} />
+                <a
+                  href="#"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2"
+                >
+                  Perfil
+                </a>
+              </div>
+            </li>
+          </ol>
+        </nav>
 
         <section className="relative">
           <div className="w-full h-64 bg-gray-400">teste</div>
@@ -35,7 +50,9 @@ const Profile = () => {
               <div>
                 <h1 className="text-2xl font-bold">Pietro Ramos</h1>
                 <p className="text-gray-700">Araçatuba - SP</p>
-                <p className="text-gray-700">Psicologo especialista não sei no que.</p>
+                <p className="text-gray-700">
+                  Psicologo especialista não sei no que.
+                </p>
                 <div className=" flex items-center justify-start gap-8 pt-6">
                   <span className="py-1 px-4 bg-blue-300 border border-blue-900 text-blue-900 rounded-md text-sm block">
                     Homem Trans
@@ -55,10 +72,10 @@ const Profile = () => {
           <div className="px-16 flex flex-col items-start justify-start gap-4">
             <h1 className="text-2xl font-bold block">Lorem ipsum dolor</h1>
             <p className="block">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum, totam?
-              Possimus nisi modi porro placeat officia ratione dolore excepturi sint
-              magni, voluptatum harum blanditiis atque ipsum corrupti, suscipit enim
-              nihil.
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum,
+              totam? Possimus nisi modi porro placeat officia ratione dolore
+              excepturi sint magni, voluptatum harum blanditiis atque ipsum
+              corrupti, suscipit enim nihil.
             </p>
           </div>
         </section>
