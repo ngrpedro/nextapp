2c50ea424b1bae93f68f34b1af24dfae87a60677
Pedro Soares <ngrpedro22@gmail.com>
2023-01-11 13:12:42 -0300

    daisyUI init

--- src/components/partners/PartnerHero.tsx
+++ src/components/partners/PartnerHero.tsx
@@ -3,9 +3,14 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="relative">
-      <div className="max-w-[63rem] m-auto ">
-        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-center font-roboto py-8">
-          Encontre instituições que estão prontas para nos receber!
+      <div className="max-w-[63rem] m-auto mt-10">
+        <h1 className="text-5xl font-extrabold text-center lg:text-7xl 2xl:text-8xl">
+          <span className="text-transparent bg-gradient-to-br bg-clip-text from-teal-500 via-indigo-500 to-sky-500">
+            Nossos
+          </span>{" "}
+          <span className="text-transparent bg-gradient-to-tr bg-clip-text from-blue-500 via-pink-500 to-red-500">
+            parceiros
+          </span>
         </h1>
 
         <p className="text-center px-10 sm:px-36 sm:py-10 font-asap text-gray-600">
@@ -16,7 +21,7 @@ const Hero = () => {
       </div>
       <div className="flex items-center justify-center py-6">
         <button className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold text-center py-2 px-8 rounded transition-colors">
-          Começar
+          Seja um de nosso parceiros
         </button>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "light",
+  },
 };
