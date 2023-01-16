683a4d123a591385de001ffb09d0803c7ac590df
Pedro Soares <ngrpedro22@gmail.com>
2023-01-16 19:49:54 -0300

    style:: first part partner form

--- src/pages/partners/addPartner/form.tsx
+++ src/pages/partners/addPartner/form.tsx
@@ -38,9 +38,9 @@ const form = () => {
       </div>
 
       <div>
-        <div className="md:grid md:grid-cols-3 md:gap-6">
+        <div className="md:grid md:grid-cols-3 md:gap-6 relative">
           <div className="md:col-span-1">
-            <div className="px-4 sm:px-0">
+            <div className="px-4 sm:px-0 sticky top-6">
               <h3 className="text-2xl tracking-tight font-extrabold text-gray-800">
                 Profile
               </h3>
@@ -53,76 +53,81 @@ const form = () => {
           <div className="mt-5 md:col-span-2 md:mt-0">
             <form action="#" method="POST">
               <div className="overflow-hidden rounded-md border border-gray-300">
-                <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label
-                        htmlFor="company-website"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Website
-                      </label>
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-                          http://
-                        </span>
-                        <input
-                          type="text"
-                          name="company-website"
-                          id="company-website"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                          placeholder="www.example.com"
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="about"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      About
-                    </label>
-                    <div className="mt-1">
-                      <textarea
-                        id="about"
-                        name="about"
-                        rows={3}
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        placeholder="you@example.com"
-                        defaultValue={""}
-                      />
-                    </div>
-                    <p className="mt-2 text-sm text-gray-500">
-                      Brief description for your profile. URLs are hyperlinked.
-                    </p>
-                  </div>
-
-                  <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-2 px-4 py-5 sm:p-6">
+                  <div className="space-y-2 col-span-1 md:col-span-2">
                     <label className="block text-sm font-medium text-gray-700">
                       Photo
                     </label>
-                    <div className="mt-1 flex items-center">
-                      <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                        <svg
-                          className="h-full w-full text-gray-300"
-                          fill="currentColor"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                        </svg>
-                      </span>
-                      <button
-                        type="button"
-                        className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                      >
-                        Change
-                      </button>
+                    <div className="flex items-center justify-start gap-8">
+                      <div className="avatar">
+                        <div className="w-24 rounded-full">
+                          <img src="https://placeimg.com/192/192/people" />
+                        </div>
+                      </div>
+                      <input
+                        type="file"
+                        className="file-input file-input-ghost w-full max-w-xs"
+                      />
                     </div>
                   </div>
 
-                  <div>
+                  <div className="space-y-2">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      className="input input-bordered w-full max-w-xs"
+                    />
+                  </div>
+
+                  <div className="space-y-2 col-span-1 md:col-span-2">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Bio
+                    </label>
+                    <textarea
+                      className="textarea textarea-bordered w-full"
+                      placeholder="Bio"
+                    ></textarea>
+                  </div>
+
+                  <div className="form-control space-y-2  col-span-1 md:col-span-2">
+                    <label
+                        htmlFor="first-name"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                      Bio
+                    </label>
+                    <label className="input-group">
+                      <span>Email</span>
+                      <input
+                        type="text"
+                        placeholder="info@site.com"
+                        className="input input-bordered  w-full"
+                      />
+                    </label>
+                  </div>
+
+                  <div className="space-y-2 col-span-1 md:col-span-2 mt-4">
                     <label className="block text-sm font-medium text-gray-700">
                       Cover photo
                     </label>
@@ -165,12 +170,9 @@ const form = () => {
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Save
-                  </button>
+                  <Link href="/partners/addPartner/form" className="btn btn-wide normal-case text-lg font-normal bg-blue-600 border-none hover:bg-blue-500">
+                    Salvar
+                  </Link>
                 </div>
               </div>
             </form>
